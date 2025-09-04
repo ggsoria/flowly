a6f03085fc38e027d34c180dca04c1bcae75b580
ggsoria <gonzalosoria.sg@gmail.com>
2025-09-04 17:53:31 -0300

    init backend

---
+++ flowly-backend/src/auth/auth.service.ts
@@ -0,0 +1,54 @@
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from 'generated/prisma/runtime/library';
+
+@Injectable()
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  async signup(dto: AuthDto) {
+    //generate the password hash
+    const hash = await argon.hash(dto.password);
+    //save the new user in the db
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          password: hash,
+        },
+      });
+      return user;
+    } catch (error) {
+      //Investigate
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+    }
+  }
+
+  async signin(dto: AuthDto) {
+    //find user if exists
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    if (!user) throw new ForbiddenException('Credentials incorrect');
+    //compare password
+    const pwMatches = await argon.verify(user.password, dto.password);
+
+    if (!pwMatches) throw new ForbiddenException('Credentials incorrect');
+
+    return user;
+  }
+
+  async test() {
+    const users = await this.prisma.user.findMany();
+    return users;
+  }
+}
